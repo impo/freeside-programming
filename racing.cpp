56b1927f63373f56b2299797fc3c157e28fc39e1
Gavin Thomas Claugus <gclaugus@gmail.com>
2015-12-13 18:06:35 -0500

    before automation

--- racing.cpp
+++ racing.cpp
@@ -69,34 +69,61 @@ vector<gem> construct_gems(int N, ld h_start)
 	return gems;
 }
 
-int main()
+vector< vector<edge> > construct_graph(int N, int R, vector<gem> &gems)
 {
-	ld N, R, W, H; /* gems, ratio, width, height */
-	cin >> N >> R >> W >> H;
-
-	vector<gem> gems(N + 1);
-
-	gems[0] = make_pair(0.0, 0.0);
-
-	for (int i = 1; i <= N; i++) {
-		gem a;
-		cin >> a.first >> a.second;
-		gems[i] = a;
-	}
-
 	vector< vector<edge> > graph(N + 1, vector<edge>());
 
 	for (int i = 0; i <= N; i++) {
 		for (int j = 0; j <= N; j++) {
-			// there exists an edge from gems[i] to gems[j]
 			if (gems[j].second > gems[i].second &&
-			   abs(slope(gems[j], gems[i])) >= R) {
+			    abs(slope(gems[j], gems[i])) >= R) {
 				edge z;
 				z.to = j;
 				graph[i].push_back(z);
 			}
 		}
 	}
+	return graph;
+}
+
+int longest_path(int N, vector< vector<edge> > &graph)
+{
+	stack<int> Stack;
+	vector<int> dist(N + 1, INT_MIN);
+	vector<bool> visited(N + 1, false);
+
+	dist[0] = 0;
+
+	for (int i = 0; i <= N; i++) {
+		if (visited[i] == false)
+			topo_sort(i, visited, Stack, graph);
+	}
+
+	while (Stack.empty() == false) {
+		int u = Stack.top();
+		Stack.pop();
+
+		if (dist[u] != INT_MIN) {
+			for (auto i = graph[u].begin(); i != graph[u].end();
+			     ++i) {
+				dist[i->to] = max(dist[i->to],
+						  (dist[u] + i->length));
+			}
+		}
+	}
+
+	auto ans = *max_element(dist.begin(), dist.end());
+	return ans;
+}
+
+int main()
+{
+	ld N, R, W, H; /* gems, ratio, width, height */
+	cin >> N >> R >> W >> H;
+
+	auto gems = construct_gems(N, 0.0);
+
+	auto graph = construct_graph(N, R, gems);
 
 	print_graph(graph, N + 1);
 
